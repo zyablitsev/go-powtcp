16b1595b6dede8132226d1d2283f2b2962542ebd
Vasiliy Zyablitsev <vasiliy.zyablitsev@gmail.com>
2024-06-21 09:10:14 +0000

    remove unused methods

--- proto/message.go
+++ proto/message.go
@@ -1,9 +1,7 @@
 package proto
 
 import (
-	"bytes"
 	"encoding/gob"
-	"fmt"
 )
 
 func init() {
@@ -27,21 +25,4 @@ const (
 type Message struct {
 	MessageType MessageType
 	Data        interface{}
-	b           []byte
-}
-
-// Bytes returns gob-encoded struct representation.
-func (s *Message) Bytes() ([]byte, error) {
-	if s.b == nil {
-		buf := bytes.Buffer{}
-		enc := gob.NewEncoder(&buf)
-		err := enc.Encode(s)
-		if err != nil {
-			err = fmt.Errorf("Message.Bytes: encode error: %w", err)
-			return nil, err
-		}
-		s.b = buf.Bytes()
-	}
-
-	return s.b[:], nil
 }
